d2bf99cb20cc46bcdb6471cc122a8fb93bdbc036
Victor Nnaji <nnajivictor0@gmail.com>
2020-07-25 04:33:36 +0100

    add gatsby config offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -229,7 +229,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-remove-serviceworker`,
+    `gatsby-plugin-offline`,
+    // `gatsby-plugin-remove-serviceworker`,
   ],
 }
